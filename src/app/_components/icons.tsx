050f8902f86f5a6444f2457c7f818b015e6403f3
TrueCapslock <ole@truecapslock.dev>
2026-06-20 19:11:47 +0200

    use user's SVG for star icon

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -34,13 +34,9 @@ export function Users({ className }: { className?: string }) {
 }
 
 export function Star({ className, filled }: { className?: string; filled?: boolean }) {
-  return filled ? (
-    <svg className={className} viewBox="0 0 24 24" width={18} height={18} fill="currentColor">
-      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-    </svg>
-  ) : (
-    <svg className={className} viewBox="0 0 24 24" width={18} height={18} fill="none" stroke="currentColor" strokeWidth={2}>
-      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+  return (
+    <svg className={className} viewBox="0 0 576 512" width={18} height={16} fill={filled ? "currentColor" : "none"} stroke="currentColor" strokeWidth={filled ? 0 : 40}>
+      <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
     </svg>
   )
 }
